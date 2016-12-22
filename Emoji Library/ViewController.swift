5afa6d0144874b6f7297de4851866d9e27180681
David Roddy <david@relishink.com>
2016-12-22 18:11:42 -0500

    Emoji Jame

--- Emoji Library/ViewController.swift
+++ Emoji Library/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    
+    @IBOutlet weak var shlubber: UITableView!
 
+    var emojis = ["🎯","🎮","🚡","🚄","🚆","🏩","💜"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        shlubber.dataSource = self
+        shlubber.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  UITableViewCell()
+        cell.textLabel?.text = "🎯"
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
